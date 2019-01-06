5eb0dff01bbe004730ce19286c80f5e15975306b
Natalie Brown <Natalie.Rawle@rackspace.com>
2019-01-05 21:54:47 -0600

    made test tabletop as example

--- pokeapi/species_test.go
+++ pokeapi/species_test.go
@@ -8,10 +8,48 @@ import (
 )
 
 func TestSpeciesLinkGet(t *testing.T) {
-	sl := SpeciesLink{
-		URL: "https://pokeapi.co/api/v2/pokemon-species/25/",
+	tt := []struct {
+		name string
+
+		expectedID int
+		expectErr  bool
+		url        string
+	}{
+		{
+			url:        "https://pokeapi.co/api/v2/pokemon-species/25/",
+			expectedID: 25,
+			name:       "as seen in pokemon return value",
+		}, {
+			url:        "https://pokeapi.co/api/v2/pokemon-species/25",
+			expectedID: 25,
+			name:       "without trailing slash",
+		}, {
+			url:       "",
+			expectErr: true,
+			name:      "empty url",
+		}, {
+			url:       "https://pokeapi.co/api/v2/pokemon-species/twenty/",
+			expectErr: true,
+			name:      "failed string conversion",
+		}, {
+			url:       "https://pokeapi.co/api/pokemon-species/25/",
+			expectErr: true,
+			name:      "bad base url",
+		},
 	}
-	species, err := sl.Get()
-	require.NoError(t, err)
-	assert.Equal(t, 25, species.ID)
+
+	for _, test := range tt {
+		t.Run(test.name, func(t *testing.T) {
+			species, err := SpeciesLink{
+				URL: test.url,
+			}.Get()
+
+			if test.expectErr {
+				assert.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, test.expectedID, species.ID)
+			}
+		})
+	}
 }
