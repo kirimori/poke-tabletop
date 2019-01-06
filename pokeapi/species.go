6eafeb6a5e7a7cd081407b1748122f74ed591be3
Natalie Brown <Natalie.Rawle@rackspace.com>
2019-01-05 21:20:26 -0600

    changed schemas to be links

--- pokeapi/species.go
+++ pokeapi/species.go
@@ -30,93 +30,51 @@ func (sl SpeciesLink) Get() (s Species, err error) {
 
 // Species is details about a pokemon species
 type Species struct {
-	BaseHappiness int `json:"base_happiness"`
-	CaptureRate   int `json:"capture_rate"`
-	Color         struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"color"`
-	EggGroups []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"egg_groups"`
+	BaseHappiness  int    `json:"base_happiness"`
+	CaptureRate    int    `json:"capture_rate"`
+	Color          Link   `json:"color"`
+	EggGroups      []Link `json:"egg_groups"`
 	EvolutionChain struct {
 		URL string `json:"url"`
 	} `json:"evolution_chain"`
-	EvolvesFromSpecies struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"evolves_from_species"`
-	FlavorTextEntries []struct {
+	EvolvesFromSpecies Link `json:"evolves_from_species"`
+	FlavorTextEntries  []struct {
 		FlavorText string `json:"flavor_text"`
-		Language   struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"language"`
-		Version struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"version"`
+		Language   Link   `json:"language"`
+		Version    Link   `json:"version"`
 	} `json:"flavor_text_entries"`
 	FormDescriptions []interface{} `json:"form_descriptions"`
 	FormsSwitchable  bool          `json:"forms_switchable"`
 	GenderRate       int           `json:"gender_rate"`
 	Genera           []struct {
 		Genus    string `json:"genus"`
-		Language struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"language"`
+		Language Link   `json:"language"`
 	} `json:"genera"`
-	Generation struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"generation"`
-	GrowthRate struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"growth_rate"`
-	Habitat struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"habitat"`
+	Generation           Link   `json:"generation"`
+	GrowthRate           Link   `json:"growth_rate"`
+	Habitat              Link   `json:"habitat"`
 	HasGenderDifferences bool   `json:"has_gender_differences"`
 	HatchCounter         int    `json:"hatch_counter"`
 	ID                   int    `json:"id"`
 	IsBaby               bool   `json:"is_baby"`
 	Name                 string `json:"name"`
 	Names                []struct {
-		Language struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"language"`
-		Name string `json:"name"`
+		Language Link   `json:"language"`
+		Name     string `json:"name"`
 	} `json:"names"`
 	Order             int `json:"order"`
 	PalParkEncounters []struct {
-		Area struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"area"`
-		BaseScore int `json:"base_score"`
-		Rate      int `json:"rate"`
+		Area      Link `json:"area"`
+		BaseScore int  `json:"base_score"`
+		Rate      int  `json:"rate"`
 	} `json:"pal_park_encounters"`
 	PokedexNumbers []struct {
-		EntryNumber int `json:"entry_number"`
-		Pokedex     struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"pokedex"`
+		EntryNumber int  `json:"entry_number"`
+		Pokedex     Link `json:"pokedex"`
 	} `json:"pokedex_numbers"`
-	Shape struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"shape"`
+	Shape     Link `json:"shape"`
 	Varieties []struct {
 		IsDefault bool `json:"is_default"`
-		Pokemon   struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"pokemon"`
+		Pokemon   Link `json:"pokemon"`
 	} `json:"varieties"`
 }
